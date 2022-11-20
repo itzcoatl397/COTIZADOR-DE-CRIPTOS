26fcd4675212b3c12131a135891e277282ff4e0c
itzcoatl <itzcoatl3b@gmail.com>
2022-11-20 16:00:23 -0600

    mensaje

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,35 +1,69 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { monedas } from "../data/monedas";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 
 export const Formulario = () => {
+  const [criptos, setCriptos] = useState([]);
+  const [error, setError] = useState(false);
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas);
-
+  const [monedaCripto, SelectCriptos] = useSelectMonedas(
+    "Elige tu Criptomoneda",
+    criptos
+  );
   useEffect(() => {
-    const consultarApi = async()=>{
-        const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10tsym=USD";
-        
-        const response = await fetch(url)
+    const consultarApi = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
 
-        const respuesta = await response.json()
-        console.log(respuesta);
-    }
-    consultarApi()
+      const response = await fetch(url);
+
+      const respuesta = await response.json();
+
+      const arrayCripto = respuesta.Data.map((cripto) => {
+        const criptos = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName,
+        };
+
+        return criptos;
+      });
+
+      setCriptos(arrayCripto);
+    };
+    consultarApi();
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([moneda, criptos].includes("")) {
+      setError(true);
+      return;
+    }
+  };
+
   return (
-    <form>
-      <SelectMonedas />
-      {moneda}
-      <input
-        type="submit"
-        value="Cotizar"
-        className="bg-indigo-500 w-full p-2.5 rounded-full 
+    <div>
+      {error && (
+        <div className="bg-red-600 w-7/6 p-2 mb-5 rounded-full">
+        
+        <h2 className="text-white font-bold text-center uppercase">Complete Los campos</h2>
+        </div>
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+        <SelectCriptos />
+
+        <input
+          type="submit"
+          value="Cotizar"
+          className="bg-indigo-500 w-full p-2.5 rounded-full 
         cursor-pointer
         text-white font-bold uppercase text-2.5xl 
          hover:bg-indigo-700 
          transition duration-3s hover:text-white"
-      />
-    </form>
+        />
+      </form>
+    </div>
   );
 };
